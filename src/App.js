ad86fd9f9f86a7f1a8c02ad459113b78cbc6c210
Bhavay Goel <goelbhavay@gmail.com>
2023-03-09 11:45:52 +0530

    Added the Input in Feed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-
+import Feed from './Feed'
 function App() {
   return (
     <div className="app"> 
@@ -12,7 +12,7 @@ function App() {
       {/* App Body */}
       <div className="app__body">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </div>
 
