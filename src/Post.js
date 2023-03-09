3e8f2de9cb81c48bd956c8cb74793630a8ad0a98
Bhavay Goel <goelbhavay@gmail.com>
2023-03-09 19:19:49 +0530

    introduced Login and Signout

--- src/Post.js
+++ src/Post.js
@@ -1,16 +1,16 @@
 import { Avatar } from '@mui/material'
-import React from 'react'
+import React, { forwardRef } from 'react'
 import InputOption from './InputOption'
 import './Post.css'
 import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined'
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined'
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined'
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
-function Post({ name, description, message, photoUrl }) {
+const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
     return (
-        <div className='post'>
+        <div ref={ref} className='post'>
             <div className="post__header">
-                <Avatar />
+                <Avatar src={photoUrl}>{name[0]}</Avatar>
                 <div className="post__info">
                     <h2>{name}</h2>
                     <p>{description}</p>
@@ -22,13 +22,13 @@ function Post({ name, description, message, photoUrl }) {
             </div>
 
             <div className="post__buttons">
-                <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray"/>
-                <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray"/>
-                <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray"/>
-                <InputOption Icon={SendOutlinedIcon} title="Send" color="gray"/>
+                <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+                <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+                <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+                <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
             </div>
         </div>
     )
 }
-
+)
 export default Post
